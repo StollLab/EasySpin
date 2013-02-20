59acfdf0a1c16e9f3d13ba6b12a6b6d876faf154
Stefan Stoll <sstoll@ucdavis.edu>
2013-02-19 20:38:19 -0800

    equivsplit: complete rewrite

--- equivsplit.m
+++ equivsplit.m
@@ -3,16 +3,16 @@
 %  Ampl = equivsplit(I,n)
 %
 %  Computes the line intensity pattern of an EPR
-%  spectrum due to an S=1/2 and n nuclear spins
+%  spectrum due to an S=1/2 and n equivalent nuclear spins
 %  with quantum number I. First-order perturbations
 %  are assumed.
 %
 %  Example:
 %
-%     Ampl = equivsplit(1/2,5)
+%     Intensity = equivsplit(1/2,5)
 %
 %  5 spins-1/2 give rise to a first-order splitting
-%  pattern Ampl = [1 5 10 10 5 1] according to
+%  pattern Intensity = [1 5 10 10 5 1] according to
 %
 %              1              0 spins
 %            1   1            1 spin-1/2
@@ -21,7 +21,7 @@
 %      1   4   6   4   1      4 spins-1/2
 %    1   5  10  10   5   1    5 spins-1/2
 
-function Ampl = equivsplit(I,n)
+function Intensity = equivsplit(I,n)
 
 if (nargin==0), help(mfilename); return; end
 
@@ -37,19 +37,17 @@ if (numel(I)~=1) || ~isreal(I) || mod(I,0.5) || (I<=0)
   error('I must be a positive multiple of 1/2.');
 end
 
-% Construct intensity pattern
-%------------------------------------------------
-% RowVec is the n-th row from a generalization of Pascal's
-% triangle. The computation is iterative, since there are
-% no closed expressions available.
-RowVec = 1;
-for iRow = 1:n
-  RowVec = [RowVec zeros(1,2*I)];
-  for iCol = length(RowVec):-1:1
-    RowVec(iCol) = sum(RowVec(max(1,iCol-2*I):iCol));
-  end
-end
+% Construct amplitude pattern
+%-------------------------------------------------------------------------
+% Intensity is the n-th row from a generalization of Pascal's triangle.
+% The computation is brute-force iterative, as I couldn't find closed
+% expressions for the generalized binomial coefficients of order 2*I+1 that
+% constitute the (n+1)-th row of the associated the generalized Pascal triangle.
 
-Ampl = RowVec;
+E = ones(1,2*I+1);
+Intensity = 1;
+for q = 1:n
+  Intensity = conv(Intensity,E);
+end
 
 return
