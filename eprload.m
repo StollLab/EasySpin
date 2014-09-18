9b836fa42eeb821bca420374aff9ec527c105e3d
Stefan Stoll <stst@uw.edu>
2014-09-17 23:04:55 -0700

    eprload: support for Adani spectrometers

--- eprload.m
+++ eprload.m
@@ -37,6 +37,7 @@
 %     SpecMan:             .d01, .exp
 %     Magnettech:          .spe (binary), .xml (xml)
 %     Active Spectrum:     .ESR
+%     Adani:               .dat
 %
 %     MAGRES:              .PLT
 %     qese, tryscore:      .eco
@@ -949,6 +950,35 @@ case {'.esr','.ESR'}
   Data = data(:,2);
   Parameters = [];
   
+case {'.dat','.DAT'}
+  %------------------------------------------------------------------
+  %   Text-based file format of Adani spectrometers
+  %------------------------------------------------------------------
+  allLines = textread(FileName,'%s','whitespace','','delimiter','\n');
+  nLines = numel(allLines);
+
+  Line1 = '======================== Parameters: ========================';
+  if ~strncmp(allLines{1},Line1,length(Line1))
+    error('The file %s is not an Adani spectrometer file',FileName);
+  end
+  Line2 = '=============================================================';
+  for idx = 1:nLines
+    found = strncmp(allLines{idx},Line2,length(Line2));
+    if found; break; end
+  end
+  if (~found)
+    error('Could not find start of data in file %s',FileName);
+  end
+  nPoints = nLines - idx;
+  data = zeros(nPoints,3);
+  for p=1:nPoints
+    L_ = allLines{idx+p};
+    L_(L_==',') = '.';
+    data(p,:) = sscanf(L_,'%f',3);
+  end
+  Abscissa = data(:,2);
+  Data = data(:,3);
+
 otherwise
   
   error('Files with extension %s not supported.',FileExtension);
