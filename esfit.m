37fc0cd31571c670c6aef25afb420450da2e9e8f
Stefan Stoll <stst@uw.edu>
2015-03-17 16:01:54 -0700

    esfit: compatibility issue with R2014b, more

--- esfit.m
+++ esfit.m
@@ -788,7 +788,7 @@ if (FitData.GUI) && (UserCommand~=99)
   % update graph
   set(findobj('Tag','expdata'),'XData',1:numel(ExpSpec),'YData',ExpSpec);
   set(findobj('Tag','bestsimdata'),'XData',1:numel(ExpSpec),'YData',real(FitData.bestspec));
-  set(findobj('Tag','currsimdata'),'XData',1:numel(ExpSpec),'YData',simspec);
+  set(findobj('Tag','currsimdata'),'XData',1:numel(ExpSpec),'YData',real(simspec));
   drawnow
   
   % update numbers parameter table
