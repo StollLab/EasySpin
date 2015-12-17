68d4283b37a24295913d45f58b71ee8cc1759dfe
Matthew Krzyaniak <mdkrzyaniak@northwestern.edu>
2015-12-17 08:40:23 -0600

    lpsvd: shifted the help above the function call to enable help when protected.

--- lpsvd.m
+++ lpsvd.m
@@ -1,4 +1,3 @@
-function [y,parameters] = lpsvd(data,time,method,order)
 % Linear Prediction Singular Value Decomposition
 %
 %  predictedSpectrum = lpsvd(Spectrum, Time, Method, Order)
@@ -26,8 +25,10 @@ function [y,parameters] = lpsvd(data,time,method,order)
 % Model Order estimated as per:
 % Wax, M.; Kailath, T.; IEEE Trans. Acoust. Speech Signal ASSP-39 387 (1985)
 
-% Check inputs
 
+function [y,parameters] = lpsvd(data,time,method,order)
+
+% Check inputs
 dim = size(data);
 
 if min(dim)>1
