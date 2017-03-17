506841383830c99ef0f4a9328ef7d1acd16d6582
Peter Martin <pr1m314@gmail.com>
2017-03-17 16:11:02 -0500

    chili: removed istospinsys and updated magint
    --HG--
    branch : magintmerge

--- easyspin/private/magint.m
+++ easyspin/private/magint.m
@@ -1,4 +1,27 @@
-function [T0,T1,T2,F0,F1,F2,isFieldDep] = magint(System,SpinOps,CenterField,IncludeNuclearZeeman,ExplicitFieldSweep)
+function varargout = magint(varargin)
+% generate the irreducible spherical tensor components and, if using the
+% general Liouvillian method, their corresponding operators as well
+
+% Make sure generalLiouvillian is fed into the first argument!
+generalLiouvillian = varargin{1};
+
+if generalLiouvillian
+  System = varargin{2};
+  SpinOps = varargin{3};
+  CenterField = varargin{4};
+  IncludeNuclearZeeman = varargin{5};
+  explicitFieldSweep = varargin{6};
+else
+  % don't need SpinOps
+  System = varargin{2};
+  CenterField = varargin{3};
+  IncludeNuclearZeeman = varargin{4};
+  explicitFieldSweep = varargin{5};
+end
+
+% Transformation from molecular frame to diffusion frame
+% (DiffFrame contains Euler angles for mol->Diff transformation)
+R_M2Diff = erot(System.DiffFrame);
 
 % Count the number of interaction terms in the spin Hamiltonian
 % -------------------------------------------------------------------------
@@ -31,7 +54,7 @@ isFieldDep = zeros(nInteractions,1);
 
 iInt = 1;
 
-if ExplicitFieldSweep
+if explicitFieldSweep
   B0 = {0 0 1}; % per Tesla
 else
   B0 = {0 0 CenterField/1e3}; % mT -> T
@@ -45,17 +68,39 @@ for iElSpin = 1:nElSpins
   else
     g = System.g((iElSpin-1)*3+(1:3),:);
   end
-  [T0{iInt},T1(iInt,:),T2(iInt,:)] = istotensor(B0,SpinOps(iElSpin,:));
+  if isfield(System,'gFrame')
+    R_g2M = erot(System.gFrame(iElSpin,:)).';
+    g = R_g2M*g*R_g2M.';  % g frame -> molecular frame
+  end
+  if ~generalLiouvillian
+    % this code wasn't originally in magint, so does the general
+    % Liouvillian scheme process a diffusion tilt elsewhere?
+    g = R_M2Diff*g*R_M2Diff.';  % molecular frame -> diffusion frame
+  end
+  if generalLiouvillian
+    [T0{iInt},T1(iInt,:),T2(iInt,:)] = istotensor(B0,SpinOps(iElSpin,:));
+  end
   [F0(iInt),F1(iInt,:),F2(iInt,:)] = istocoeff(g*bmagn/planck); % Hz
   isFieldDep(iInt) = true;
+  if ~generalLiouvillian
+    % Generate custom fields for Freed method
+    if any(abs(F1(iInt,:))>1e-6)
+      error('g tensor must be symmetric for this method.');
+    end
+    % Set parameters for chili_liouvmatrix*
+    System.g_axial(iElSpin) = F2(iInt,1)==0;
+    System.EZ0(iElSpin) = F0(iInt)*(CenterField/1e3)*2*pi; % Hz -> angular frequency
+    System.EZ2(:,iElSpin) = F2(iInt,:).'*(CenterField/1e3)*2*pi; % Hz -> angular frequency
+  end
   iInt = iInt + 1;
 end
+
+noEZbetatilt = all(F2(1:iInt-1,[2 4])==0);
 
 % Hyperfine interaction terms (S*A*I)
 %--------------------------------------------------------------------------
 for iElSpin = 1:nElSpins
   eidx = 3*(iElSpin-1)+(1:3);
-  S_ = SpinOps(iElSpin,:);
   for iNucSpin = 1:nNucSpins
     if System.fullA
       A_ = System.A(3*(iNucSpin-1)+(1:3),eidx)*1e6; % MHz -> Hz
@@ -67,10 +112,28 @@ for iElSpin = 1:nElSpins
       R_A2M = R_M2A.'; % A frame -> mol frame
       A_ = R_A2M*A_*R_A2M.';
     end
-    I_ = SpinOps(nElSpins+iNucSpin,:);
-    [T0{iInt},T1(iInt,:),T2(iInt,:)] = istotensor(S_,I_);
+    if ~generalLiouvillian
+      % this code wasn't originally in magint, so does the general
+      % Liouvillian scheme process a diffusion tilt elsewhere?
+      A_ = R_M2Diff*A_*R_M2Diff.';  % molecular frame -> diffusion frame
+    end
+    if generalLiouvillian
+      S_ = SpinOps(iElSpin,:);
+      I_ = SpinOps(nElSpins+iNucSpin,:);
+      [T0{iInt},T1(iInt,:),T2(iInt,:)] = istotensor(S_,I_);
+    end
     [F0(iInt),F1(iInt,:),F2(iInt,:)] = istocoeff(A_);
     isFieldDep(iInt) = false;
+    if ~generalLiouvillian
+      % Generate custom fields for Freed method
+      if (any(abs(F1(iInt,:))>1e-6))
+        error('Hyperfine tensors must be symmetric for this method.');
+      end
+      % Set parameters for chili_liouvmatrix*
+      System.A_axial(iNucSpin,iElSpin) = F2(iInt,1)==0;
+      System.HF0(iNucSpin,iElSpin) = F0(iInt)*2*pi; % Hz -> angular frequency
+      System.HF2(:,iNucSpin,iElSpin) = F2(iInt,:).'*2*pi; % Hz -> angular frequency
+    end
     iInt = iInt + 1;
   end
 end
@@ -94,8 +157,10 @@ if (nZFS>0)
       D_ = R_D2M*D_*R_D2M.';
     end
     % Compute ISTO components
-    S_ = SpinOps(iSpin,:);
-    [T0{iInt},T1(iInt,:),T2(iInt,:)] = istotensor(S_,S_);
+    if generalLiouvillian
+      S_ = SpinOps(iSpin,:);
+      [T0{iInt},T1(iInt,:),T2(iInt,:)] = istotensor(S_,S_);
+    end
     [F0(iInt),F1(iInt,:),F2(iInt,:)] = istocoeff(D_);
     isFieldDep(iInt) = false;
     iInt = iInt + 1;
@@ -119,7 +184,9 @@ if nElSpins>1
         R_ee2M = R_M2ee.';  % ee frame -> mol frame
         J_ = R_ee2M*diag(System.ee(iCoupling,:))*R_ee2M.';
       end
-      [T0{iInt},T1(iInt,:),T2(iInt,:)] = istotensor(SpinOps(iEl1,:),SpinOps(iEl2,:));
+      if generalLiouvillian
+        [T0{iInt},T1(iInt,:),T2(iInt,:)] = istotensor(SpinOps(iEl1,:),SpinOps(iEl2,:));
+      end
       [F0(iInt),F1(iInt,:),F2(iInt,:)] = istocoeff(J_*1e6); % MHz -> Hz
       isFieldDep(iInt) = false;
       iInt = iInt + 1;
@@ -130,14 +197,34 @@ end
 
 % Nuclear Zeeman interaction terms (-muN*B*gn*I/h)
 %--------------------------------------------------------------------------
-if IncludeNuclearZeeman
-  for iNucSpin = 1:nNucSpins
-    I_ = SpinOps(nElSpins+iNucSpin,:);
-    gn_ = System.gn(iNucSpin);
-    [T0{iInt},T1(iInt,:),T2(iInt,:)] = istotensor(B0,I_);
-    [F0(iInt),F1(iInt,:),F2(iInt,:)] = istocoeff(-gn_*nmagn/planck);
-    isFieldDep(iInt) = true;
-    iInt = iInt + 1;
+if generalLiouvillian
+  if IncludeNuclearZeeman
+    for iNucSpin = 1:nNucSpins
+        I_ = SpinOps(nElSpins+iNucSpin,:);
+        gn_ = System.gn(iNucSpin);
+        [T0{iInt},T1(iInt,:),T2(iInt,:)] = istotensor(B0,I_);
+        [F0(iInt),F1(iInt,:),F2(iInt,:)] = istocoeff(-gn_*nmagn/planck);
+        isFieldDep(iInt) = true;
+        iInt = iInt + 1;
+    end
+  end
+else
+  if IncludeNuclearZeeman
+    gn0 = zeros(nNucSpins,1);
+    for iNucSpin = 1:nNucSpins
+      gn0(iNucSpin) = istocoeff(System.gn(iNucSpin));
+      System.NZ0(iNucSpin) = nmagn*(CenterField/1e3)*gn0(iNucSpin)/planck*2*pi; % -> angular freq.
+    end
+  else
+    System.NZ0 = zeros(1,nNucSpins);
+  end
+
+  % Adaption for two nuclei, to feed to chili_liouvmatrix2
+  if (System.nNuclei>=2)
+    System.Ib = System.I(2);
+    System.NZ0b = System.NZ0(2);
+    System.HF0b = System.HF0(2);
+    System.HF2b = System.HF2(:,2);
   end
 end
 
@@ -145,18 +232,47 @@ end
 %--------------------------------------------------------------------------
 if IncludeNuclearQuadrupole
   for iNucSpin = 1:nNucSpins
-    I_ = SpinOps(nElSpins+iNucSpin,:);
     Q_ = System.Q(iNucSpin,:)*1e6; % MHz -> Hz
     if any(System.QFrame(iNucSpin,:))
       R_M2Q = erot(System.QFrame(iNucSpin,:)); % mol frame -> Q frame
       R_Q2M = R_M2Q.'; % Q frame -> mol frame
       Q_ = R_Q2M*diag(Q_)*R_Q2M.';
     end
-    [T0{iInt},T1(iInt,:),T2(iInt,:)] = istotensor(I_,I_);
+    if generalLiouvillian
+      I_ = SpinOps(nElSpins+iNucSpin,:);
+      [T0{iInt},T1(iInt,:),T2(iInt,:)] = istotensor(I_,I_);
+    end
     [F0(iInt),F1(iInt,:),F2(iInt,:)] = istocoeff(Q_);
     isFieldDep(iInt) = false;
     iInt = iInt + 1;
   end
+end
+
+% Symmmetry tests
+%--------------------------------------------------------------------------
+% no need for including System.nNuclei==0 here since all interaction tensor
+% components will be parsed, regardless of the number of nuclei
+Symmetry.nobetatilts = allclose(F2(:,[2 4]),0);
+Symmetry.tensorsCollinear = all(isreal(F2));
+Symmetry.axialSystem = allclose(F2(:,1),0);
+
+% Output
+%--------------------------------------------------------------------------
+
+% Pack up ISTOs
+T.T0 = T0;
+T.T1 = T1;
+T.T2 = T2;
+
+% Pack up ISTs
+F.F0 = F0;
+F.F1 = F1;
+F.F2 = F2;
+
+if generalLiouvillian
+  varargout = {T,F,Symmetry,isFieldDep};
+else
+  varargout = {System,Symmetry};
 end
 
 return
