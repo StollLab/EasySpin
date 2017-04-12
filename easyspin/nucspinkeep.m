8fdf47c875d5beb212c8b7df417543c3a2e65709
Stefan Stoll <stst@uw.edu>
2017-04-12 13:14:38 -0700

    add new function nucspinkeep

---
+++ easyspin/nucspinkeep.m
@@ -0,0 +1,37 @@
+% nucspinkeep  Remove nuclear spins from spin system
+%
+%   NewSys = nucspinrmv(Sys,keepidx)
+%
+%   Removes one or more nuclei from the spin system
+%   Sys and returns the result in NewSys. keepidx a vector
+%   of nuclear indices as they appear in Sys. E.g. keepidx=1
+%   removes all except the first nucleus, and keepidx=[2 4]
+%   removes all except the second and the fourth nucleus.
+%
+%   Example:
+%    Sys.Nucs = '13C,1H,14N,14N';
+%    Sys.A = [14 5 7 8.2];
+%    Sys = nucspinkeep(Sys,2);  % keeps only 1H
+
+function NewSys = nucspinkeep(Sys,keepidx)
+
+if (nargin==0), help(mfilename); return; end
+
+NewSys = Sys;
+
+if (nargin<2), return; end
+if isempty(keepidx), return; end
+
+Nucs = nucstring2list(NewSys.Nucs);
+nNuclei = numel(Nucs);
+
+if any(keepidx>nNuclei) || any(keepidx<=0)
+  error('There are only %d nuclei in the spin system. Index out of range.',nNuclei);
+end
+
+rmvidx = 1:nNuclei;
+rmvidx(keepidx) = [];
+
+NewSys = nucspinrmv(Sys,rmvidx);
+
+return
---
+++ tests/nucspinkeep_basic.m
@@ -0,0 +1,13 @@
+function [err,data] = test(opt,olddata)
+
+Sys = struct('S',1/2,'g',[2 2.1 2.2]);
+Sys = nucspinadd(Sys,'14N',[1 2 3]);
+Sys = nucspinadd(Sys,'1H',[23 34 45]);
+
+Sys1  = nucspinkeep(Sys,1);
+Sys2  = nucspinkeep(Sys,2);
+Sys12 = nucspinkeep(Sys,[1 2]);
+
+err = size(Sys1.A,1)~=1 || size(Sys2.A,1)~=1 || size(Sys12.A,1)~=2;
+
+data = [];
