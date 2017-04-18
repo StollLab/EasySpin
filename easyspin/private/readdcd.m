7fcbd0a349655d03a9b1c6a3ef1cc373f27c96ba
Peter Martin <pr1m314@gmail.com>
2017-04-18 09:53:30 -0500

    readdcd: replaced multiple fclose() calls with the safer onCleanup() option
    --HG--
    branch : cardamom

--- easyspin/private/readdcd.m
+++ easyspin/private/readdcd.m
@@ -25,16 +25,16 @@ switch nargin
     fileName = varargin{1};
     idx = [];
   case 2
-    fileName = varargin{1};
+    FileName = varargin{1};
     idx = varargin{2};
     nidx = length(idx);
 end
 
-if ~ischar(fileName)
+if ~ischar(FileName)
   error('Filename must be given as a character array.')
 end
 
-if ~endsWith(fileName,'.dcd')
+if ~endsWith(FileName,'.dcd')
   error('Please give the full filename including the ".dcd" extension.')
 end
 
@@ -43,131 +43,129 @@ if islogical(idx)
 end
 
 % obtain integer file identifier
-fileID = fopen(fileName, 'r');
+FileID = fopen(FileName, 'r');
+if FileID<1, error('File "%s" could not be opened.', FileName); end
+ensurefclose = onCleanup(@() fclose(FileID));
 
 % check size of file
-fseek(fileID, 0, 'eof');
-fileSize = ftell(fileID);
-fseek(fileID, 0, 'bof');
+fseek(FileID, 0, 'eof');
+fileSize = ftell(FileID);
+fseek(FileID, 0, 'bof');
 
 % check endianness of binary file
 % TODO can we just check machineformat and be done?
-[fileName, ~, machineformat] = fopen(fileID);
+[FileName, ~, machineformat] = fopen(FileID);
 
 % block 1
 % -------------------------------------------------------------------------
-header.blocksize1 = fread(fileID, 1, 'int32');
+header.blocksize1 = fread(FileID, 1, 'int32');
 
 % TODO figure out why this needs to be 84
 if header.blocksize1 ~= 84
-  fclose(fileID);
-  error('The following file does not have proper format:\n"%s"', fileName)
+  error('File "%s" does not have proper format.', FileName)
 end
 
 % CORD for coordinates or VELD for velocities
-header.HDR = fread(fileID, 4, 'char');
+header.HDR = fread(FileID, 4, 'char');
 
 % number of frames
-header.NSET = fread(fileID, 1, 'int32');
+header.NSET = fread(FileID, 1, 'int32');
 
 % starting time step
-header.ISTRT = fread(fileID, 1, 'int32');
+header.ISTRT = fread(FileID, 1, 'int32');
 
 % trajectory saving frequency
-header.NSAVC = fread(fileID, 1, 'int32');
+header.NSAVC = fread(FileID, 1, 'int32');
 
 % total number of time steps
-header.NSTEP = fread(fileID, 1, 'int32');
+header.NSTEP = fread(FileID, 1, 'int32');
 
 % null4 (int*4)
-header.NULL4 = fread(fileID, 4, 'int32');
+header.NULL4 = fread(FileID, 4, 'int32');
 
 % number of free atoms
-header.NFREAT = fread(fileID, 1, 'int32');
+header.NFREAT = fread(FileID, 1, 'int32');
 
 % size of time step
-header.DELTA = fread(fileID, 1, 'float32');
+header.DELTA = fread(FileID, 1, 'float32');
 
 % null9 (int*9)
-header.NULL9 = fread(fileID, 9, 'int32');
+header.NULL9 = fread(FileID, 9, 'int32');
 
 % version
-header.VERSION = fread(fileID, 1, 'int32');
+header.VERSION = fread(FileID, 1, 'int32');
 
 % check for charmm format
 if header.VERSION > 0
   header.isCHARMM = 1;
   
-  cof = ftell(fileID);
+  cof = ftell(FileID);
 
   % CHARMM extrablock
-  fseek(fileID, 48, 'bof');
-  if fread(fileID, 1, 'int32')==1
+  fseek(FileID, 48, 'bof');
+  if fread(FileID, 1, 'int32')==1
     header.CHARMMextrablock = 1;
   end
   
   % CHARMM 4dims
-  fseek(fileID, 52, 'bof');
-  if fread(fileID, 1, 'int32')==1
+  fseek(FileID, 52, 'bof');
+  if fread(FileID, 1, 'int32')==1
     header.CHARMMextrablock = 1;
   end
 
-  fseek(fileID, cof, 'bof');
+  fseek(FileID, cof, 'bof');
 
 else
   % xplor format
   header.isCHARMM = 0;
   
-  cof = ftell(fileID);
-  fseek(fileID, 44, 'bof');
-  header.DELTA = fread(fileID, 1, 'float64');
-  fseek(fileID, cof, 'bof');
+  cof = ftell(FileID);
+  fseek(FileID, 44, 'bof');
+  header.DELTA = fread(FileID, 1, 'float64');
+  fseek(FileID, cof, 'bof');
 end
 
 % check consistency
-blocksize1 = fread(fileID, 1, 'int32');
+blocksize1 = fread(FileID, 1, 'int32');
 if header.blocksize1~=blocksize1
-  fclose(fileID);
   error('Inconsistent sizes for block 1.')
 end
 
 % block 2 (title)
 % -------------------------------------------------------------------------
 % blocksize2
-header.blocksize2 = fread(fileID, 1, 'int32');
+header.blocksize2 = fread(FileID, 1, 'int32');
 
 % # of title lines
-header.ntitle = fread(fileID, 1, 'int32');
+header.ntitle = fread(FileID, 1, 'int32');
 
 % title
-header.TITLE = fread(fileID, 80*header.ntitle, 'char');
+header.TITLE = fread(FileID, 80*header.ntitle, 'char');
 header.TITLE = char(reshape(header.title, 80, [])');
 
 % check consistency
-blocksize2 = fread(fileID, 1, 'int32');
+blocksize2 = fread(FileID, 1, 'int32');
 if header.blocksize2~=blocksize2
-  fclose(fileID);
   error('Inconsistent sizes for block 2.')
 end
 
 % block 3 (natom)
 % -------------------------------------------------------------------------
 % blocksize3
-header.blocksize3 = fread(fileID, 1, 'int32');
+header.blocksize3 = fread(FileID, 1, 'int32');
 
 % # of atoms
-header.NATOM = fread(fileID, 1, 'int32');
+header.NATOM = fread(FileID, 1, 'int32');
 
 % check consistency
-blocksize3 = fread(fileID, 1, 'int32');
+blocksize3 = fread(FileID, 1, 'int32');
 if header.blocksize3~=blocksize3
-  fclose(fileID);
   error('Inconsistent sizes for block 3.')
 end
 
 % coordinates
 % -------------------------------------------------------------------------
-headersize = ftell(fileID);
+headersize = ftell(FileID);
 
 if header.CHARMMextrablock
   extrablocksize = 4*2 + 8*6;
@@ -190,25 +188,25 @@ box = zeros(nFrames, 3);
 for iFrame = 1:nFrames
   % charmm extrablock (unitcell info)
   if header.CHARMMextrablock
-    blocksize = fread(fileID, 1, 'int32');
-    dummy = fread(fileID, 6, 'float64');
-    blocksize = fread(fileID, 1, 'int32');
+    blocksize = fread(FileID, 1, 'int32');
+    dummy = fread(FileID, 6, 'float64');
+    blocksize = fread(FileID, 1, 'int32');
   end
 
   % x coordinates
-  blocksize = fread(fileID, 1, 'int32');
-  x = fread(fileID, blocksize/4, 'float32');
-  blocksize = fread(fileID, 1, 'int32');
+  blocksize = fread(FileID, 1, 'int32');
+  x = fread(FileID, blocksize/4, 'float32');
+  blocksize = fread(FileID, 1, 'int32');
 
   % y coordinates 
-  blocksize = fread(fileID, 1, 'int32');
-  y = fread(fileID, blocksize/4, 'float32');
-  blocksize = fread(fileID, 1, 'int32');
+  blocksize = fread(FileID, 1, 'int32');
+  y = fread(FileID, blocksize/4, 'float32');
+  blocksize = fread(FileID, 1, 'int32');
 
   % z coordinates 
-  blocksize = fread(fileID, 1, 'int32');
-  z = fread(fileID, blocksize/4, 'float32');
-  blocksize = fread(fileID, 1, 'int32');
+  blocksize = fread(FileID, 1, 'int32');
+  z = fread(FileID, blocksize/4, 'float32');
+  blocksize = fread(FileID, 1, 'int32');
   
 %   % ignore charmm 4dims extension
 %   if header.CHARMM4dims
@@ -224,7 +222,5 @@ for iFrame = 1:nFrames
   traj(iFrame, 2:3:end) = y(idx)';
   traj(iFrame, 3:3:end) = z(idx)';
 end
-  
-fclose(fileID);
 
 end
