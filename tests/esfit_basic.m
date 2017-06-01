cee3896329a2273ecf23891d9a0368948cc80de1
Peter Martin <pr1m314@gmail.com>
2017-06-01 14:05:18 -0500

    esfit tests: added checks against rmsd value of esfit output
    --HG--
    branch : esfit-updates

--- tests/esfit_basic.m
+++ tests/esfit_basic.m
@@ -9,13 +9,22 @@ Exp.Field = 350;
 Exp.mwRange = [9.5 10.5];
 
 [nu,spc] = pepper(Sys,Exp);
+rng(1,'twister')
 spc = addnoise(spc,50,'u');
 
 Vary.g = [0.02 0.02]; 
 Opt = struct;
 FitOpt.PrintLevel = 0;
 FitOpt.Method = 'levmar fcn';
-[bestsys,spcfit] = esfit('pepper',spc,Sys,Vary,Exp,Opt,FitOpt);
+[~,spcfit,resid] = esfit('pepper',spc,Sys,Vary,Exp,Opt,FitOpt);
+
+err = false;
+
+rmsd = sqrt(mean(resid.^2));
+
+if rmsd>3e-2
+  err = 1;
+end
 
 if opt.Display
   plot(nu,spc,nu,spcfit);
@@ -23,4 +32,3 @@ if opt.Display
 end
 
 data = [];
-err = false;
