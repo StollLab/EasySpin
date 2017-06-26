7404c636faf748dfec8c1308ecb5a4328bd22181
Peter Martin <pr1m314@gmail.com>
2017-06-26 12:39:04 -0500

    Manually revert esfit_multisys to resolve conflict default branch (source)
    --HG--
    branch : esfit-updates

--- tests/esfit_multisys.m
+++ tests/esfit_multisys.m
@@ -1,6 +1,6 @@
 function [err,data] = test(opt,olddata)
 
-% Assure that esfit runs using multiple systems.
+% Assure that esfit runs.
 
 Sys1.g = [2 2.1];
 Sys1.lw = 10;
@@ -11,7 +11,6 @@ Exp.Field = 350;
 Exp.mwRange = [9.5 10.5];
 
 [nu,spc] = pepper({Sys1,Sys2},Exp);
-rng_(1,'twister')
 spc = addnoise(spc,80,'u');
 
 Vary1.g = [0.02 0.02]; 
@@ -19,15 +18,7 @@ Vary2.g = [0.02 0.02];
 Opt = struct;
 FitOpt.PrintLevel = 0;
 FitOpt.Method = 'levmar fcn';
-[~,spcfit,resid] = esfit('pepper',spc,{Sys1,Sys2},{Vary1,Vary2},Exp,Opt,FitOpt);
-
-err = false;
-
-rmsd = sqrt(mean(resid.^2));
-
-if rmsd>3e-2
-  err = 1;
-end
+[dummy,spcfit] = esfit('pepper',spc,{Sys1,Sys2},{Vary1,Vary2},Exp,Opt,FitOpt);
 
 if opt.Display
   plot(nu,spc,nu,spcfit);
@@ -35,3 +26,4 @@ if opt.Display
 end
 
 data = [];
+err = false;
