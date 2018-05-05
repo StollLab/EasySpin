d1416b9adabd5c7dc9ed6f39d8aa1c57cfe4b9e9
Stefan Stoll <stst@uw.edu>
2018-05-04 23:37:56 -0700

    chili: extended diffusion operator function

--- easyspin/private/diffsuperop.m
+++ easyspin/private/diffsuperop.m
@@ -1,25 +1,35 @@
-% This function computes the diffusion superoperator matrix in the LMKjK basis,
-% for a diagonal diffusion tensor. It does not support ordering potentials.
-% This function does not require a particular order of spatial basis functions.
+% This function computes the diffusion superoperator matrix in the LMK or LMKjK basis,
+% for a diagonal diffusion tensor. It does not require any particular order of
+% spatial basis functions. It does not support ordering potentials.
 %
-%   Diff     principal values of diffusion tensor (s^-1)
-%   basis    list of L,M,K,jK quantum numbers for spatial basis, one set per row
-%   Gamma    diffusion superoperator in the given basis
+% Input:
+%   R        array with 3 principal values of diffusion tensor (s^-1)
+%   basis    list of (L,M,K) or (L,M,K,jK) quantum numbers for orientational
+%            basis, one set per row
+%
+% Output:
+%   Gamma    diffusion superoperator in the given LMK or LMKjK basis (s^-1)
 
-function Gamma = diffsuperop(Diff,basis)
+function Gamma = diffsuperop(R,basis)
 
 nBasis = size(basis,1);
 
-L   = basis(:,1);
-M   = basis(:,2);
-K   = basis(:,3);
-jK  = basis(:,4);
+L = basis(:,1);
+M = basis(:,2);
+K = basis(:,3);
 
-Rx = Diff(1);
-Ry = Diff(2);
-Rz = Diff(3);
-Rd = 0.25*(Rx-Ry);
-Rperp = 0.5*(Rx+Ry);
+jKbasis = size(basis,2)==4;
+if jKbasis
+  jK = basis(:,4);
+else
+  jK = zeros(nBasis,1);
+end
+
+Rx = R(1);
+Ry = R(2);
+Rz = R(3);
+Rd = (Rx-Ry)/4;
+Rperp = (Rx+Ry)/2;
 
 idx = 1;
 for b1 = 1:nBasis
@@ -39,16 +49,25 @@ for b1 = 1:nBasis
     if (L1~=L2), break; end
     if (M1~=M2), continue; end
     if (abs(K1-K2) > 2), continue; end
+    
     if (jK1~=jK2), continue; end
     
     K2zero = (K2==0);
     
-    if (K2==K1)
+    if (K1==K2)
       val_ = Rperp*(L1*(L1+1)-K1^2) + Rz*K1^2;
-    elseif (K2==K1+2)
-      val_ = Rd*sqrt((1+K1zero)*(1+K2zero)*(L1+K2-1)*(L1+K2)*(L1-K2+1)*(L1-K2+2));
-    elseif (K2==K1-2)
-      val_ = Rd*sqrt((1+K1zero)*(1+K2zero)*(L1-K2-1)*(L1-K2)*(L1+K2+1)*(L1+K2+2));
+    elseif (K1==K2+2)
+      if jKbasis
+        val_ = Rd*sqrt((1+K1zero)*(1+K2zero)*(L1-K2-1)*(L1-K2)*(L1+K2+1)*(L1+K2+2));
+      else
+        val_ = Rd*sqrt((L1-K2-1)*(L1-K2)*(L1+K2+1)*(L1+K2+2));
+      end
+    elseif (K1==K2-2)
+      if jKbasis
+        val_ = Rd*sqrt((1+K1zero)*(1+K2zero)*(L1+K2-1)*(L1+K2)*(L1-K2+1)*(L1-K2+2));
+      else
+        val_ = Rd*sqrt((L1+K2-1)*(L1+K2)*(L1-K2+1)*(L1-K2+2));
+      end
     else
       continue
     end
