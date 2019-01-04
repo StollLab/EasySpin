bfe801a5f415fb9a0f1fc072ed41f98afb7dd0a1
Peter Martin <pr1m314@gmail.com>
2019-01-04 17:36:33 -0500

    Split cardamom_markov_emghmm_2d into direct and regression tests
    --HG--
    branch : cardamom

---
+++ tests/cardamom_markov_emghmm_2d_direct.m
@@ -0,0 +1,165 @@
+function [err,data] = test(opt,olddata)
+% Direct test on means and transmats for multivariate Gaussian HMM 
+% expectation maximization function in 2D for periodic boundary conditions
+
+rng(1)
+
+% Simulate state trajectory using dynamic parameters
+% -------------------------------------------------------------------------
+
+nDims = 2;
+nStates = 3;
+nRepeats = 5;
+verbosity = 0;
+
+Sys.TransProb = [  0.7, 0.1, 0.2; 
+                   0.1, 0.8, 0.1;
+                   0.1, 0.3, 0.6 ];
+Par.nTraj = 1;
+Par.dt = 1e-9;
+Par.nSteps = 10000;
+Opt.statesOnly = true;
+[~,stateTraj] = stochtraj_jump(Sys,Par,Opt);
+
+testData = zeros(Par.nSteps, nDims);
+
+% Create observation trajectory using state trajectory and normal 
+% distributions assigned to each state
+% -------------------------------------------------------------------------
+
+% Width of normal distributions 
+width = 20/180*pi;
+
+% Centers of normal distributions
+muTrue = [   0,   0; 
+              150, 150; 
+             -150,   0 ]/180*pi;
+
+for iStep = 1:Par.nSteps
+  state = stateTraj(iStep);
+  testData(iStep,:) = width*randn(1,2) + muTrue(state,:);
+end
+
+% Wrap data according to periodic boundary conditions
+testDataWrapped = testData;
+testDataWrapped(testData>pi) = testData(testData>pi) - 2*pi;
+testDataWrapped(testData<-pi) = testData(testData<-pi) + 2*pi;
+
+% Perform clustering using random seeds
+% -------------------------------------------------------------------------
+
+centroids0 = [];
+[idxBest, centroidsBest] = runprivate('cardamom_kmeans',...
+                                      testDataWrapped,nStates,nRepeats,centroids0,verbosity);
+
+                                    
+% Train HMM parameters against observation trajectory
+% -------------------------------------------------------------------------
+
+% Initialize the means and covariance matrices for the HMM
+mu0 = centroidsBest.';
+Sigma0 = zeros(nDims,nDims,nStates);
+for iState = 1:nStates
+  idxState = idxBest==iState;
+  Sigma0(:,:,iState) = cov_pbc(testData(idxState,:), mu0(:,iState).', 2*pi);
+end
+
+% Initialize TPM and equilibrium distribution using uniform distributions
+transmat0 = rand(nStates);
+transmat0 = transmat0./sum(transmat0,2);
+
+eqdistr0 = rand(nStates,1);
+eqdistr0 = eqdistr0./sum(eqdistr0,1);
+
+% EM function wants an array of shape (nDims,nSteps,nTraj)
+testDataWrapped = permute(testDataWrapped,[2,1,3]);
+
+% Run expectation maximization algorithm
+[eqdistr1,transmat1,mu1,Sigma1,~] = ...
+    runprivate('cardamom_emghmm',testDataWrapped,eqdistr0,transmat0,mu0,Sigma0,[],verbosity);
+
+% Compare
+% -------------------------------------------------------------------------
+
+err = 0;
+
+% Assignment of output state indices might not correspond to input state
+% indices, so check which ordering gives the best agreement and re-assign
+% indices accordingly
+idxPerm = perms([1,2,3]);
+bestAgreement = inf;
+for k = 1:size(idxPerm,1)
+  idx = idxPerm(k,:);
+  thisAgreement = max(max(abs(mu1(:,idx)-muTrue')));
+  if thisAgreement < bestAgreement
+    bestAgreement = thisAgreement;
+    bestidx = idx;
+  end
+end
+
+% Reorder HMM parameters based on best agreement
+mu1 = mu1(:,bestidx);
+temp = transmat1(:,bestidx);
+transmat1 = temp(bestidx,:);
+
+muDiff = abs(mu1 - muTrue');
+transmatChange = abs(Sys.TransProb - transmat1)./transmat1;
+
+muThreshold = 5/180*pi;
+transmatThreshold = 0.5;
+
+if    (max(muDiff(:)) > muThreshold) ...
+   || (max(transmatChange(:)) > transmatThreshold)
+  err = 1;
+end
+
+data = [];
+
+if opt.Display
+  
+  hold on
+  % scatter(testDataWrapped(1,:), testDataWrapped(2,:), 10, 'filled')
+  for iState = 1:nStates
+    idxState = idxBest==iState;
+    scatter(testDataWrapped(1,idxState), testDataWrapped(2,idxState), 10, 'filled')
+  end
+%   scatter(mu0(1,:), mu0(2,:), 40, 'filled')
+  scatter(muTrue(:,1), muTrue(:,2), 40, 'filled')
+  scatter(mu1(1,:), mu1(2,:), 40, 'green')
+  hold off
+  
+end
+
+end
+
+% Helper functions
+% -------------------------------------------------------------------------
+
+function c = cov_pbc(x, mu, W)
+
+[nPoints,nDims] = size(x);
+    
+% remove the centers
+xc = zeros(nPoints,nDims);
+for iPoint = 1:nPoints
+  xc(iPoint,:) = dist_pbc(x(iPoint,:)-mu, W);
+end
+
+c = (xc' * xc) ./ nPoints;
+
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function dist = dist_pbc(dist,W)
+
+w = W/2;
+
+% dist = w - abs(pi - abs(dist));
+
+idx1 = dist > w;
+idx2 = dist < -w;
+
+dist(idx1) = dist(idx1) - W;
+dist(idx2) = dist(idx2) + W;
+
+end
