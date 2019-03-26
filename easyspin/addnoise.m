6dce992f678df8e6565af4634c1d6de62650ddf8
Stefan Stoll <stst@uw.edu>
2019-03-25 21:13:04 -0700

    addnoise: refactor
    --HG--
    branch : Polarization

--- easyspin/addnoise.m
+++ easyspin/addnoise.m
@@ -19,65 +19,84 @@
 %     yn = addnoise(y,20);
 %     plot(x,yn);
 
-function yn = addnoise(y,SNR,model)
+function yn = addnoise(y,SNR,noisemodel)
 
-if (nargin==0), help(mfilename); return; end
+if nargin==0, help(mfilename); return; end
 
-if (nargin<3), model = 'f'; end
+if nargin<3, noisemodel = 'f'; end
 
-if numel(SNR)~=1 || (SNR<=0)
+if numel(SNR)~=1 || SNR<=0
   error('The second input, the signal-to-noise ratio (SNR), must be a positive number.');
 end
 
-%rand('state',sum(100*clock)); % obsoleted in R2011a
+if ~any(noisemodel=='unf')
+  error('The third input, the noise model, must be ''u'', ''n'', or ''f''.');
+end
 
 dims = size(y);
-% check for complex data
-iscpx = double(~isreal(y));
-for cpx=0:iscpx
-  if cpx, rnoise = noise;end
-  switch model
-    case 'u',
-      noise = rand(dims)-0.5;
-      
-    case 'n',
-      noise = randn(dims);
-      
-    case 'f', % general 1/f^alpha noise
-      alpha = 1;
-      
-      % construct frequency domain with 1/f^alpha power characteristics
-      % and random phases
-      nPoints = numel(y);
-      freq = 0:floor(nPoints/2);
-      Amplitudes = freq.^(-alpha/2);
-      Amplitudes(1) = 1;
-      Phases = exp(2i*pi*rand(size(Amplitudes)));
-      FreqDomain = Amplitudes.*Phases;
-      if mod(nPoints,2)
-        d = [FreqDomain conj(FreqDomain(end:-1:2))];
-      else
-        d = [FreqDomain conj(FreqDomain(end-1:-1:2))];
-      end
-      noise = real(ifft(d));
-      isColVec = dims(1)>dims(2);
-      if isColVec, noise = noise(:); end
-      
-    otherwise
-      error('Wrong noise model. Use ''n'', ''u'', or ''f''.');
-  end
-  if cpx, noise = complex(rnoise,noise);end
-end
-noise = noise/std(noise(:)); % scale to stddev = 1;
-noise = reshape(noise,size(y));
 
-signallevel = (max(y(:))-min(y(:)));
+complexData = ~isreal(y); % check for complex data
+
+switch noisemodel
+  case 'u' % uniform distribution [-0.5,0.5]
+    rnoise = rand(dims)-0.5;
+    if complexData
+      inoise = rand(dims)-0.5;
+    end
+  case 'n' % normal distribution with mean 0 and standard dev. 1
+    rnoise = randn(dims);
+    if complexData
+      inoise = randn(dims);
+    end
+  case 'f' % general 1/f^alpha noise
+    alpha = 1;
+    rnoise = oneoverfnoise(dims,alpha);
+    if complexData
+      inoise = oneoverfnoise(dims,alpha);
+    end
+  otherwise
+    error('Wrong noise model. Use ''n'', ''u'', or ''f''.');
+end
+  
+if complexData
+  noise = complex(rnoise,inoise);
+else
+  noise = rnoise;
+end
+
+noise = noise/std(noise(:)); % scale to stddev = 1
+noise = reshape(noise,dims);
+
+signallevel = max(y(:)) - min(y(:));
 signallevel = max([real(signallevel) imag(signallevel)]);
 
-if (signallevel>0)
+if signallevel>0
   noiselevel = signallevel/SNR;
 else
   noiselevel = 1;
 end
 
 yn = y + noise*noiselevel;
+
+return
+%===============================================================================
+
+% construct frequency domain with 1/f^alpha power characteristics and random phases
+function noise = oneoverfnoise(dims,alpha)
+
+nPoints = prod(dims);
+freq = 0:floor(nPoints/2);
+Amplitudes = freq.^(-alpha/2);
+Amplitudes(1) = 1;
+Phases = exp(2i*pi*rand(size(Amplitudes)));
+FreqDomain = Amplitudes.*Phases;
+if mod(nPoints,2)
+  d = [FreqDomain conj(FreqDomain(end:-1:2))];
+else
+  d = [FreqDomain conj(FreqDomain(end-1:-1:2))];
+end
+noise = real(ifft(d));
+isColVec = dims(1)>dims(2);
+if isColVec, noise = noise(:); end
+
+return
