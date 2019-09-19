333ae18548bebdeb4a65decd955f42fb946fbdad
Stefan Stoll <stst@uw.edu>
2019-09-19 12:52:39 -0700

    evolve: better documentation (fixes #53)

--- easyspin/evolve.m
+++ easyspin/evolve.m
@@ -1,4 +1,4 @@
-% evolve  Time domain evolution of density matrix
+% evolve  Time-domain evolution of density matrix
 %
 %   td = evolve(Sig,Det,Ham,n,dt);
 %   td = evolve(Sig,Det,Ham,n,dt,IncScheme);
@@ -6,47 +6,53 @@
 %
 %   Evolves the density matrix Sig under the Hamiltonian Ham with time
 %   step dt n-1 times and detects using Det after each step. Hermitian
-%   input matrices are assumed. td(1) is the value obtained by detecting
-%   Sig without evolution.
+%   input matrices are assumed. Ham is assumed to be in units of MHz.
+%
+%   n gives the number of points along each dimension (a number for 1D
+%   experiments, a 2-vector for 2D experiments). dt contains the time increment
+%   along each dimension, in microseconds.
 %
 %   IncScheme determines the incrementation scheme and can be one of the
-%   following (up to four sweep periods, up to two dimensions)
+%   following (up to five incrementation periods, up to two dimensions)
 %
-%     [1]           simple FID, 3p-ESEEM, echo transient, DEFENCE
-%     [1 1]         2p-ESEEM, CP, 3p and 4p RIDME
-%     [1 -1]        3p-DEER, 4p-DEER, PEANUT, 5p RIDME
-%     [1 1 -1 -1]   SIFTER
-%     [1 -1 -1 1]   7p-DEER
-%     [1 -1 1 -1]
+%     1D experiments:
+%       [1]           simple FID, 3p-ESEEM, echo transient, DEFENCE
+%       [1 1]         2p-ESEEM, CP, 3p- and 4p-RIDME
+%       [1 -1]        3p-DEER, 4p-DEER, PEANUT, 5p-RIDME
+%       [1 -1 1 -1]
+%       [1 1 -1 -1]   SIFTER
+%       [1 -1 -1 1]   7p-DEER
 %
-%     [1 2]         3p-ESEEM echo transient, HYSCORE, DONUT-HYSCORE
-%     [1 2 1]       2D 3p-ESEEM
-%     [1 1 2]       2p-ESEEM etc. with echo transient
-%     [1 -1 2]      3p-DEER, 4p-DEER etc. with echo transient
-%     [1 2 2 1]     2D CP
-%     [1 2 -2 1]    2D PEANUT
-%     [1 1 -1 -1 2] SIFTER with echo transient
-%     [1 -1 -1 1 2] 7p-DEER with echo transient
+%     2D experiments:
+%       [1 2]         3p-ESEEM echo transient, HYSCORE, DONUT-HYSCORE
+%       [1 1 2]       2p-ESEEM etc. with echo transient
+%       [1 -1 2]      3p-DEER, 4p-DEER etc. with echo transient
+%       [1 2 1]       2D 3p-ESEEM
+%       [1 2 2 1]     2D CP
+%       [1 2 -2 1]    2D PEANUT
+%       [1 1 -1 -1 2] SIFTER with echo transient
+%       [1 -1 -1 1 2] 7p-DEER with echo transient
 %
-%   [1] is the default. For an explanation of the format, see the
-%   documentation.
+%   [1] is the default. 1 and -1 indicate incrementation and decrementation
+%   along the first dimensions, and 2 and -2 are analogous for the second
+%   dimensions. For more details, see the documentation.
 %
 %   Mix is a cell array containing the propagators of the mixing
-%   block(s), for experiments with more than 1 sweep period.
+%   block(s). It is required for experiments with more than 1 sweep period.
 %
-%   td is a vector/matrix of the signal with t1 along dimension 1
-%   and t2 along dimension 2.
+%   td is the output signal, a vector for 1D experiments and a matrix for
+%   2D experiments. td(1) is the value obtained by detecting Sig without evolution.
 
 function Signal = evolve(Sig,Det,Ham,n,dt,IncScheme,Mix)
 
-if (nargin==0), help(mfilename); return; end
+if nargin==0, help(mfilename); return; end
 
-if (nargout<1), error('Not enough output arguments.'); end
-if (nargout>1), error('Too many output arguments.'); end
-if (nargin<5) || (nargin>7), error('Wrong number of input arguments!'); end
+if nargout<1, error('Not enough output arguments.'); end
+if nargout>1, error('Too many output arguments.'); end
+if nargin<5 || nargin>7, error('Wrong number of input arguments!'); end
 
-if (nargin<6), IncScheme = 1; end
-if (nargin<7), Mix = {}; end
+if nargin<6, IncScheme = 1; end
+if nargin<7, Mix = {}; end
 
 if any(mod(n,1)~=0) || any(n<=0)
   error('n, the number of points (4th argument), must be a positive integer.');
@@ -54,11 +60,14 @@ end
 
 % IncScheme check
 %------------------------------------------------------------
-if (length(IncScheme)>1) && (nargin<7),
+if length(IncScheme)>1 && nargin<7
   error('The requested IncScheme requires mixing propagators, but none are provided!');
 end
+if ~isnumeric(IncScheme) || ~isvector(IncScheme) || ~isreal(IncScheme)
+  error('IncScheme (6th input) must be a vector containing 1, -1, 2, and -2.');
+end
 if any((abs(IncScheme)~=1) & (abs(IncScheme)~=2))
-  error('IncScheme can contain only 1, -1, 2, and -2.');
+  error('IncScheme (6th input) can contain only 1, -1, 2, and -2.');
 end
 
 nEvolutionPeriods = numel(IncScheme);
@@ -76,12 +85,12 @@ if ~iscell(Mix)
 end
 nMixingBlocks = numel(Mix);
 
-if (nMixingBlocks~=nEvolutionPeriods-1),
+if nMixingBlocks~=nEvolutionPeriods-1
   error('Number of mixing propagators not correct! %d are needed.',nEvolutionPeriods-1);
 end
 N = size(Sig,1);
 
-if (nDimensions==1)
+if nDimensions==1
   for iDet = 1:nDetectors
     Signal{iDet} = zeros(n,1);
   end
@@ -120,7 +129,7 @@ if ~iscell(Ham)
   end
   
   % Define free evolution propagators
-  if (nDimensions==1)
+  if nDimensions==1
     diagU = exp(-2i*pi*dt*E);
   else
     diagUX = exp(-2i*pi*dt(1)*E);
@@ -180,7 +189,8 @@ if nDetectors==1
   Detector = Detector{1};
 end
 
-if isequal(IncScheme,1) % IncScheme [1]
+if isequal(IncScheme,1)
+  
   FinalDensity = Density(:);
   U_ = diagU*diagU';
   U_ = U_(:);
@@ -196,7 +206,8 @@ if isequal(IncScheme,1) % IncScheme [1]
     FinalDensity = U_.*FinalDensity; % equivalent to U*FinalDensity*U'
   end
   
-elseif isequal(IncScheme,[1 1]) % IncScheme [1 1]
+elseif isequal(IncScheme,[1 1])
+  
   UU_ = diagU*diagU.';
   % It is not necessary to evolve the initial density matrix.
   % Only the mixing propagator needs to be evolved.
@@ -215,7 +226,8 @@ elseif isequal(IncScheme,[1 1]) % IncScheme [1 1]
     Mix1 = UU_.*Mix1; % equivalent to U*Mix1*U
   end
   
-elseif isequal(IncScheme,[1 -1]) % IncScheme [1 -1]
+elseif isequal(IncScheme,[1 -1])
+  
   % Pre-propagate mixing propagator to end of second period (= start of
   % experiment)
   MixX = diag(diagU.^n)*Mix{1};
@@ -232,7 +244,8 @@ elseif isequal(IncScheme,[1 -1]) % IncScheme [1 -1]
     MixX = UtU_.*MixX; % equivalent to U^-1*MixX*U
   end
   
-elseif isequal(IncScheme,[1 2]) % IncScheme [1 2]
+elseif isequal(IncScheme,[1 2])
+  
   UX_ = diagUX*diagUX';
   UY_ = diagUY*diagUY';
   UY_ = reshape(UY_,N^2,1);
@@ -252,7 +265,8 @@ elseif isequal(IncScheme,[1 2]) % IncScheme [1 2]
     Density = UX_.*Density; % equivalent to UX*Densty*UX';
   end
   
-elseif isequal(IncScheme,[1 1 2]) % IncScheme [1 1 2]
+elseif isequal(IncScheme,[1 1 2])
+  
   UUX_ = diagUX*diagUX.';
   UY_ = diagUY*diagUY';
   Mix1 = Mix{1};
@@ -273,7 +287,8 @@ elseif isequal(IncScheme,[1 1 2]) % IncScheme [1 1 2]
     Mix1 = UUX_.*Mix1; % equivalent to UX*Mix1*UX
   end
   
-elseif isequal(IncScheme,[1 -1 2]) % IncScheme [1 -1 2]
+elseif isequal(IncScheme,[1 -1 2])
+  
   UtUX_ = conj(diagUX)*diagUX.';
   UY_ = diagUY*diagUY';
   % Pre-propagate mixing propagator to end of second period (= start of
@@ -296,7 +311,8 @@ elseif isequal(IncScheme,[1 -1 2]) % IncScheme [1 -1 2]
     MixX = UtUX_.*MixX; % equivalent to U^-1*MixX*U
   end
   
-elseif isequal(IncScheme,[1 2 1]) % IncScheme [1 2 1]
+elseif isequal(IncScheme,[1 2 1])
+  
   Mix1 = Mix{1};
   Mix2 = Mix{2};
   UUX_ = diagUX*diagUX.';
@@ -318,7 +334,8 @@ elseif isequal(IncScheme,[1 2 1]) % IncScheme [1 2 1]
     Mix1 = UY*Mix1;
   end
   
-elseif isequal(IncScheme,[1 2 2 1]) % IncScheme [1 2 2 1]
+elseif isequal(IncScheme,[1 2 2 1])
+  
   Mix1 = Mix{1};
   Mix2 = Mix{2};
   Mix3 = Mix{3};
@@ -341,7 +358,8 @@ elseif isequal(IncScheme,[1 2 2 1]) % IncScheme [1 2 2 1]
     Mix2 = UUY_.*Mix2; % equivalent to UY*Mix2*UY
   end
   
-elseif isequal(IncScheme,[1 2 -2 1]) % IncScheme [1 2 -2 1]
+elseif isequal(IncScheme,[1 2 -2 1])
+  
   Mix1 = Mix{1};
   Mix2 = diag(diagUY.^n(2))*Mix{2}; % pre-propagate to endpoint of third delay
   Mix3 = Mix{3};
@@ -364,7 +382,8 @@ elseif isequal(IncScheme,[1 2 -2 1]) % IncScheme [1 2 -2 1]
     Mix2 = UtUY_.*Mix2; % equivalent to UY'*Mix2*UY
   end
   
-elseif isequal(IncScheme,[1 -1 1 -1]) % IncScheme [1 -1 1 -1]
+elseif isequal(IncScheme,[1 -1 1 -1])
+  
   Mix1X = diag(diagU.^n)*Mix{1}; % pre-propagate to endpoint of second delay
   Mix2 = Mix{2};
   Mix3X = diag(diagU.^n)*Mix{3}; % pre-propagate to endpoint of fourth delay
@@ -383,7 +402,8 @@ elseif isequal(IncScheme,[1 -1 1 -1]) % IncScheme [1 -1 1 -1]
     Mix3X = UtU_.*Mix3X; % equivalent to U'*Mix3X*U
   end
   
-elseif isequal(IncScheme,[1 1 -1 -1]) % IncScheme [1 1 -1 -1]
+elseif isequal(IncScheme,[1 1 -1 -1])
+  
   Mix1 = Mix{1};
   Mix2X = diag(diagU.^n)*Mix{2}; % pre-propagate to endpoint of third delay
   Mix3X = diag(diagU.^n)*Mix{3}; % pre-propagate to endpoint of fourth delay
@@ -403,7 +423,29 @@ elseif isequal(IncScheme,[1 1 -1 -1]) % IncScheme [1 1 -1 -1]
     Mix3X = UU3_.*Mix3X; % equivalent to U'*Mix3X*U'
   end
    
-elseif isequal(IncScheme,[1 1 -1 -1 2]) % IncScheme [1 1 -1 -1 2]
+elseif isequal(IncScheme,[1 -1 -1 1])
+  
+  Mix1X = diag(diagU.^n)*Mix{1}; % pre-propagate to endpoint of second delay
+  Mix2 = Mix{2};
+  Mix3X = Mix{3}*diag(diagU.^n); % forward-propagate to endpoint of fourth delay
+  UtU1_ = conj(diagU)*diagU.'; % propagator for Mix1 (add before, remove after)
+  UtU3_ = diagU*diagU'; % propagator for Mix3 (remove before, add after)
+  for ix = 1:n
+    MixX = Mix3X*Mix2*Mix1X;
+    FinalDensity = MixX*Density*MixX';
+    if nDetectors==1
+      Signal(ix) = Detector*FinalDensity(:);
+    else
+      for iDet = 1:nDetectors
+        Signal{iDet}(ix) = Detector{iDet}*FinalDensity(:);
+      end
+    end
+    Mix1X = UtU1_.*Mix1X; % equivalent to U'*Mix1X*U
+    Mix3X = UtU3_.*Mix3X; % equivalent to U*Mix3X*U'
+  end
+  
+elseif isequal(IncScheme,[1 1 -1 -1 2])
+  
   Mix1 = Mix{1};
   Mix2X = diag(diagUX.^n(1))*Mix{2}; % pre-propagate to endpoint of third delay
   Mix3X = diag(diagUX.^n(1))*Mix{3}; % pre-propagate to endpoint of fourth delay
@@ -428,27 +470,8 @@ elseif isequal(IncScheme,[1 1 -1 -1 2]) % IncScheme [1 1 -1 -1 2]
     Mix3X = UU3_.*Mix3X; % equivalent to U'*Mix3X*U'
   end
   
-elseif isequal(IncScheme,[1 -1 -1 1]) % IncScheme [1 -1 -1 1]
-  Mix1X = diag(diagU.^n)*Mix{1}; % pre-propagate to endpoint of second delay
-  Mix2 = Mix{2};
-  Mix3X = Mix{3}*diag(diagU.^n); % forward-propagate to endpoint of fourth delay
-  UtU1_ = conj(diagU)*diagU.'; % propagator for Mix1 (add before, remove after)
-  UtU3_ = diagU*diagU'; % propagator for Mix3 (remove before, add after)
-  for ix = 1:n
-    MixX = Mix3X*Mix2*Mix1X;
-    FinalDensity = MixX*Density*MixX';
-    if nDetectors==1
-      Signal(ix) = Detector*FinalDensity(:);
-    else
-      for iDet = 1:nDetectors
-        Signal{iDet}(ix) = Detector{iDet}*FinalDensity(:);
-      end
-    end
-    Mix1X = UtU1_.*Mix1X; % equivalent to U'*Mix1X*U
-    Mix3X = UtU3_.*Mix3X; % equivalent to U*Mix3X*U'
-  end
+elseif isequal(IncScheme,[1 -1 -1 1 2])
   
-elseif isequal(IncScheme,[1 -1 -1 1 2]) % IncScheme [1 -1 -1 1 2]
   Mix1X = diag(diagUX.^n(1))*Mix{1}; % pre-propagate to endpoint of second delay
   Mix2 = Mix{2};
   Mix3X = Mix{3}*diag(diagUX.^n(1)); % forward-propagate to endpoint of fourth delay
@@ -474,7 +497,10 @@ elseif isequal(IncScheme,[1 -1 -1 1 2]) % IncScheme [1 -1 -1 1 2]
   end
   
 else
-  error('Unsupported incrementation scheme!');
+  
+  IncSchemeString = sprintf('%d ',IncScheme);
+  error('The incrementation scheme\n  [%s]\ngiven in IncScheme is not supported.',IncSchemeString);
+  
 end
 
 return
