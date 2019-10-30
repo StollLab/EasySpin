9edd688dcfeba774be4750239618d37bdbb079a7
Stefan Stoll <stst@uw.edu>
2019-10-30 09:28:37 -0700

    rework order parameter example

--- examples/slowmotion/orderparameter.m
+++ examples/slowmotion/orderparameter.m
@@ -1,56 +1,49 @@
 % Calculate order parameter S(2,0)
 %===============================================================================
 
-% How to calculate an order parameter S(2,0) given an orientation potential
-% coefficient (lambda_200 or C_200, i.e. L = 2, M = 0, and K = 0.
+% This example shows calculates an order parameter S(2,0) given an orientation
+% potential coefficient (lambda_200 or C_200, i.e. L = 2, M = 0, and K = 0).
 
 clear, clc, clf
 
 % Set up orienational potential and population functions
 %-------------------------------------------------------------------------------
-theta = linspace(0,pi/2,1001); % radians
-ct = cos(theta);
 
-% orientations (pseudo)potential coefficient
-C20 = 5; % unitless
-Y20 = (3*ct.^2-1)*sqrt(5/pi)/4; % the associated orientational function
+% orientation potential coefficient and basis function
+lambda20 = 2; % coefficient, unitless (multiples of kT)
+Y20 = @(theta) (3*cos(theta).^2-1)*sqrt(5/pi)/4; % the associated orientational function
 
 % set up the potential and the resulting probability distribution
 T = 293; % temperature, K
 kT = boltzm*293; % J
-U = -kT*C20*Y20; % potential energy function
-P = exp(-U/kT); % probability distribution (unnormalized)
-
-% normalize the probability distribution
-dtheta = theta(2)-theta(1);
-Z = trapz(P.*sin(theta))*dtheta; % partition function
-P = P/Z;
+U = @(theta) -kT*lambda20*Y20(theta); % potential energy function
+Z = integral(@(theta)exp(-U(theta)/kT).*sin(theta),0,pi/2); % partition function
+P = @(theta) exp(-U(theta)/kT)/Z; % probability distribution (normalized)
 
 % Calculation of ordering parameter for L=2, K=0
 %-------------------------------------------------------------------------------
-% basis function for axial order: Y_(2,0)
-% (is the simplest possible; can be normalized or not)
-Y20 = (3*ct.^2-1)*sqrt(5/pi)/4;
-
 % To get order parameter S_(2,0), compute the integral
 %                   \int_0^pi P(theta) Y20(theta) sin(theta) dtheta
 % S_(2,0) = <Y20> = -----------------------------------------------
 %                          \int_0^pi P(theta) sin(theta) dtheta
+%
+% The denominator integral is Z, and we already included it in P.
 
-S20 = trapz(P.*Y20.*sin(theta))*dtheta;
+S20 = integral(@(theta)P(theta).*Y20(theta).*sin(theta),0,pi/2);
+
 
 % Plotting
 %-------------------------------------------------------------------------------
+theta = linspace(0,pi/2,1001); % radians
+
 subplot(2,1,1)
-plot(theta*180/pi,U/echarge);
-set(gca,'FontSize',12);
-xlabel('theta (deg)');
-ylabel('potential energy (eV)');
+plot(theta*180/pi,P(theta));
+xlabel('\theta (deg)');
+ylabel('probability density');
+ylim([0 max(P(theta))]);
+title(sprintf('\\lambda_{20} = %g    S_{20} = %g',lambda20,S20));
 
 subplot(2,1,2)
-plot(theta*180/pi,P);
-set(gca,'FontSize',12);
-xlabel('theta (deg)');
-ylabel('probability density');
-ylim([0 max(P)]);
-legend(sprintf('S20 = %g',S20));
+plot(theta*180/pi,U(theta)/echarge);
+xlabel('\theta (deg)');
+ylabel('potential energy (eV)');
