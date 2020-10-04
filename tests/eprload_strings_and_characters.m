665e3cecbf513f184f2c6daae5fad4b11641f509
Stephan Pribitzer <stephapr@uw.edu>
2020-10-04 14:54:25 -0700

    eprload not correctly loading files (#198)
    * fix a bug where exist no longer works for a string array and fileparts no longer returns empty file extensions
    issue #197
    
    * add test to verify eprload processes string and character vectors
    
    * slight tweak to test

---
+++ tests/eprload_strings_and_characters.m
@@ -0,0 +1,13 @@
+function ok = test()
+
+FilePaths = {'./eprfiles/00012107','./eprfiles/00011201',"./eprfiles/00012107","./eprfiles/00012107.dta","./eprfiles/00011201","./eprfiles/00011201.spc"};
+
+try
+  for i = 1 : length(FilePaths)
+    [~,~] = eprload(FilePaths{i});
+  end
+  ok = true;
+catch
+  ok = false;
+end
+
