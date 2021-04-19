ff7866cb6817c5d5187f86956717fec8022f7120
Stefan Stoll <stst@uw.edu>
2021-04-19 16:34:09 -0700

    esfit: add test for parameter standard derivation

---
+++ tests/esfit_std.m
@@ -0,0 +1,36 @@
+function ok = test()
+
+% Test whether esfit() returns correct confidence intervals (CIs).
+% This test compares esfit()'s CI values to CI values obtained from
+% MATLAB's cftool and DeerLab's fitparamodel (which are consistent).
+
+data = textread('esfit_std_data.txt'); %#ok<DTXTRD>
+t = data(:,1);
+y = data(:,2);
+
+pmodel = [2 0.3 0.2];
+lb = [0.1 0.1 -2];
+p0 = [1.5 0.3 0.5];
+ub = [10 0.5 2];
+
+fcn = @(p) modelfun(t,p);
+
+FitOpt = struct;
+FitOpt.Method = 'simplex fcn';
+FitOpt.PrintLevel = 1;
+FitOpt.Scaling = 'none';
+
+result = esfit(y,fcn,p0,lb,ub,FitOpt);
+
+% Reference parameter standard deviations, determined using MATLAB's
+% Curve Fitting Tool and DeerLab's fitparamodel.
+pstd_ref = [0.0149, 0.0043, 0.0053];
+
+ok(1) = areequal(result.pfit,pmodel.',3e-2,'abs');
+ok(2) = areequal(result.pstd,pstd_ref.',2e-2,'rel');
+
+end
+
+function y = modelfun(t,p)
+y = p(1)*exp(-p(2)*t)+p(3);
+end
