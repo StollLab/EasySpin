5ef57299ccfd7786c3ca68ff8fc648cfcef7d3b1
mdkrzyaniak <58000065+mdkrzyaniak@users.noreply.github.com>
2021-07-22 10:48:04 -0500

    Added D_ field, tests and docu
    Added D_ which is defined as [D E/D] so that the ratio E/D can be held constant for fitting and simulation. Includes the basic tests and updates to the documentation. Issue #49

--- tests/zfield_rhombic.m
+++ tests/zfield_rhombic.m
@@ -5,5 +5,9 @@ function ok = test()
 Sys = struct('S',1,'D',[3 1]);
 H0a = [1 0 1; 0 -2 0; 1 0 1];
 H0b = zfield(Sys);
+ok(1) = areequal(H0a,H0b,1e-12,'abs');
 
-ok = areequal(H0a,H0b,1e-12,'abs');
+Sys = struct('S',1,'D_',[3 1/3]);
+H0a = [1 0 1; 0 -2 0; 1 0 1];
+H0b = zfield(Sys);
+ok(2) = areequal(H0a,H0b,1e-12,'abs');
