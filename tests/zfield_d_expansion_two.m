425933555f42bb2dfc69a88fa8d1ced7743f3eff
Matthew Krzyaniak <58000065+mdkrzyaniak@users.noreply.github.com>
2021-08-16 23:48:00 -0500

    Introduce Sys.D_ to support E/D (#221)
    * Update resfreqs_matrix.m
    
    resfreqs_matrix: minor bugfix with spin polarization
    
    * Fixed a couple documentation typos.
    
    * Added Gaussian pulse cascade universal rotations
    
    * Polarization documentation and example update
    
    Polarization is now no longer an undocumented feature.
    
    * Added D_ field, tests and docu
    
    Added D_ which is defined as [D E/D] so that the ratio E/D can be held constant for fitting and simulation. Includes the basic tests and updates to the documentation. Issue #49
    
    * Revert "Polarization documentation and example update"
    
    This reverts commit db77b0080ae61cfb25df49bd882fa8c957c12f92.

--- tests/zfield_d_expansion_two.m
+++ tests/zfield_d_expansion_two.m
@@ -16,7 +16,12 @@ H2 = zfield(Sys);
 Sys.D = D(:)*[-1,-1,2]/3;
 H3 = zfield(Sys);
 
+Sys = rmfield(Sys,'D');
+Sys.D_ = [D(1) 0; D(2), 0];
+H4 = zfield(Sys);
+
 % test
 threshold = 1e-7;
 ok(1) = areequal(H1,H2,threshold,'abs');
 ok(2) = areequal(H1,H3,threshold,'abs');
+ok(3) = areequal(H1,H4,threshold,'abs');
