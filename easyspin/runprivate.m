7a30b98dec163a1016f00813063aa3acfaa7db45
Stefan Stoll <stst@uw.edu>
2022-06-22 09:08:10 -0700

    runprivate(): small edit, add test

--- easyspin/runprivate.m
+++ easyspin/runprivate.m
@@ -8,14 +8,11 @@
 %   arg1, arg2  function arguments
 %
 % Output:
-%   varargout outputs returned from function call
+%   outputs returned from function call
 
 function varargout = runprivate(fname,varargin)
 
-result = cell(1,nargout);
-
-[result{:}] = feval(fname,varargin{:});
-
-varargout = result;
+varargout = cell(1,nargout);
+[varargout{:}] = feval(fname,varargin{:});
 
 end
