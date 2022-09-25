9cd14047b7608d53360c2808a1aa8797ab0fb8bd
Stefan Stoll <stst@uw.edu>
2022-09-24 23:03:36 -0700

    fix ham_cf documentation

--- easyspin/ham_cf.m
+++ easyspin/ham_cf.m
@@ -1,57 +1,56 @@
 % ham_cf  Crystal-field Hamiltonian for the orbital angular momentum
 %
-%   F = ham_cf(SpinSystem)
-%   F = ham_cf(SpinSystem,OAMs)
-%   F = ham_cf(SpinSystem,OAMs,'sparse')
+%   H = ham_cf(Sys)
+%   H = ham_cf(Sys,OAMs)
+%   H = ham_cf(Sys,OAMs,'sparse')
 %
-%   Returns the crystal-field Hamiltonian (in MHz) of the system
-%   SpinSystem.
+%   Returns the crystal-field Hamiltonian (in MHz) of the spin system
+%   Sys, utilizing the fields Sys.L, Sys.CF1, Sys.CF2, etc.
 %
 %   If the vector OAMs is given, the crystal field of only the
-%   specified orbital angular momentums is returned (1 is the first, 2 the
-%   second, etc). Otherwise, all orbital angular momentums are included.
+%   specified orbital angular momenta is returned (1 is the first in Sys.L,
+%   2 the second, etc). Otherwise, all orbital angular momenta are included.
 %
 %   If 'sparse' is given, the matrix is returned in sparse format.
 
-function H = ham_cf(SpinSystem,idxOAM,opt)
+function H = ham_cf(SpinSystem,idxL,opt)
 
 if nargin==0, help(mfilename); return; end
 
-[Sys,err] = validatespinsys(SpinSystem);
-error(err);
-
-if nargin<2, idxOAM = []; end
+if nargin<2, idxL = []; end
 if nargin<3, opt = ''; end
+
 if ~ischar(opt)
   error('Third input must be a string, ''sparse''.');
 end
 useSparseMatrices = strcmp(opt,'sparse');
 
-if isempty(idxOAM)
-  idxOAM = 1:Sys.nElectrons;
-end
+[Sys,err] = validatespinsys(SpinSystem);
+error(err);
 
-if any(idxOAM>Sys.nElectrons) || any(idxOAM<1)
+if isempty(idxL)
+  idxL = 1:Sys.nL;
+end
+if any(idxL>Sys.nL) || any(idxL<1)
   error('OAM index/indices (2nd argument) out of range!');
 end
 
 H = sparse(Sys.nStates,Sys.nStates);
-% index of OAM starts with 1, but in Sys.Spins and in H, they appear after 
-% electron spin and nuclei
-offset = Sys.nElectrons+Sys.nNuclei;
+
 % if no orbital angular momentum is defined, H remains all zero  
-if numel(Sys.Spins)==offset
+if Sys.nL==0
   if ~useSparseMatrices
     H = full(H);
   end
-  return;
+  return
 end
-OAM = Sys.Spins(1+offset:end);
 
-for iOAM = idxOAM
+offset = Sys.nElectrons+Sys.nNuclei;  % skip over electrons and nuclei in stev()
+L = Sys.L;
+for iL = idxL
   
-  % L < 1: no zero-field interaction possible
-  if OAM(iOAM)<1, continue; end
+  % L < 1: no crystal-field interaction possible
+  if L(iL)<1, continue; end
 
   % High-order terms in extended Stevens operator format
   %---------------------------------------------------------
@@ -66,14 +65,14 @@ for iOAM = idxOAM
     CFk = Sys.(fieldname);
     if isempty(CFk), continue; end
     
-    if numel(CFk)~=Sys.nElectrons && numel(CFk)~=(2*k+1)*Sys.nElectrons
-      error('Sys.%s has wrong size. It should contain 1 or %d elements',fieldname,2*k+1);
+    if numel(CFk)~=(2*k+1)*Sys.nL
+      error('Sys.%s has wrong size. It should contain %d elements for each L.',fieldname,2*k+1);
     end
     if all(CFk==0), continue; end
     
     q = k:-1:-k;
-    for iq = find(CFk(iOAM,:)~=0)
-      H = H + CFk(iOAM,iq)*stev(Sys.Spins,[k,q(iq),iOAM+offset]);
+    for iq = find(CFk(iL,:)~=0)
+      H = H + CFk(iL,iq)*stev(Sys.Spins,[k,q(iq),iL+offset]);
     end
     
   end % for all tensor ranks
@@ -81,8 +80,9 @@ for iOAM = idxOAM
 end % for all electron spins specified
 
 H = (H+H')/2; % Hermitianise
+
 if ~useSparseMatrices
   H = full(H);
 end
 
-return
+end
