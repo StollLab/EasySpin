ad28afd296d35ef6528015b71d29ea13626a61dd
Stefan Stoll <stst@uw.edu>
2022-11-09 13:02:24 -0800

    fix rapidscan2spc test

--- tests/rapidscan2spc_basic.m
+++ tests/rapidscan2spc_basic.m
@@ -1,21 +1,27 @@
-function [ok,data] = rs2spc_basic(opt,olddata)
+function [ok,data] = test(opt,olddata)
 
 % Simulate rapid-scan signal
 g = 2;
-T1 = 4;    % microseconds
-T2 = 0.5;  % microseconds
+T1 = 4;         % µs
+T2 = 0.5;       % µs
 B1 = 1e-3;      % mT
 deltaB0 = 0;    % mT
-ModAmp_mT = 1;   % mT
-ModFreq = 50;    % kHz  
+modAmp = 1;     % mT
+modFreq = 50;   % kHz
 
-[t,Mx,My,Mz] = blochsteady(g,T1,T2,deltaB0,B1,ModAmp_mT,ModFreq);
+Opt.nPoints = 1000;
+
+[t,Mx,My,Mz] = blochsteady(g,T1,T2,deltaB0,B1,modAmp,modFreq,Opt);
 M = -Mx + 1i*My;
 
-
-[B,spc] = rapidscan2spc(M,ModAmp_mT,ModFreq,2);
+[B,spc] = rapidscan2spc(M,modAmp,modFreq,2);
 
 data.spc = spc;
+
+if opt.Display
+  plot(B,real(spc),B,real(olddata.spc));
+  legend('current','previous');
+end
 
 if ~isempty(olddata)
   ok = areequal(data.spc,olddata.spc,1e-6,'rel');
