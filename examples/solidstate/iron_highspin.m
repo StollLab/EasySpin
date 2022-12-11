a84538ab1ba02bf9c608e4d1639e97b7fa6ff765
Stefan Stoll <stst@uw.edu>
2022-12-11 12:10:49 -0800

    improve Fe(III) example

--- examples/solidstate/iron_highspin.m
+++ examples/solidstate/iron_highspin.m
@@ -3,29 +3,30 @@
 clear, clc
 
 % Experimental parameters
-Exp.mwFreq = 9.5; % GHz
-Exp.Range = [0 800]; % mT - wide sweep
-Exp.Temperature = 10; % K- don't forget to include the temperature!
-Exp.nPoints = 5e3; % lots of points for a wide sweep
+Exp.mwFreq = 9.5;      % GHz
+Exp.Range = [0 700];   % mT - wide sweep
+Exp.Temperature = 10;  % K - don't forget to include the temperature!
+Exp.nPoints = 5e3;     % lots of points for a wide sweep
 
-% Approach 1: S=5/2 system with zero-field spliting and isotropic g
-Fe1.S = 5/2;
-Fe1.g = 2;
-cm2MHz = 29e3; % conversion from cm^-1 to MHz
-Fe1.D = [1 0.05]*10*cm2MHz; % MHz
-Fe1.lwpp = 5; % mT
-[B,spc1] = pepper(Fe1,Exp);
+% Approach 1: full S=5/2 system with zero-field spliting and isotropic g
+Fe_full.S = 5/2;            % 5 unpaired electrons
+Fe_full.g = 2;              % isotropic g
+Fe_full.D = [10 0.5]*30e3;  % cm^-1 -> MHz
+Fe_full.lwpp = 5;           % mT
 
-% Approach 2: effective S=1/2 system with anisotropic g
-% (This only works for |D|>>kB*T)
-Fe2.S = 1/2;
-Fe2.g = [7.13 4.78 1.919];
-Fe2.lwpp = Fe1.lwpp;
-[B,spc2] = pepper(Fe2,Exp);
+[B,spc1] = pepper(Fe_full,Exp);
+
+% Approach 2: effective S=1/2 system with anisotropic g (only works for |D|>>kB*T)
+Fe_eff.S = 1/2;
+Fe_eff.g = [7.13 4.78 1.919];
+Fe_eff.lwpp = Fe_full.lwpp;
+
+[B,spc2] = pepper(Fe_eff,Exp);
 
 % Plotting
 plot(B,spc1,B,spc2);
 grid on
-legend('S=5/2','S_{eff}=1/2');
+legend('S = 5/2','S_{eff} = 1/2');
+legend boxoff
 xlabel('magnetic field (mT)');
 ylabel('intensity (arb.u.)');
