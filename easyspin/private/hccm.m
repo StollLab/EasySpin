adde1fc5eec25c341253b509dcfa73ad6892a7ad
Stefan Stoll <stst@uw.edu>
2022-12-20 10:26:43 -0800

    esfit: reduce memory use in covariance matrix estimation (fixes #274)

--- easyspin/private/hccm.m
+++ easyspin/private/hccm.m
@@ -9,11 +9,13 @@ function C = hccm(J,res,mode)
 %   a given LSQ problem given by the Jacobian matrix (J) and the covariance
 %   matrix of the data (V). If the residual (res) is specified, the
 %   covariance matrix is estimated using some of the methods specified in
-%   (mode). The HCCM matrices are valid for both heteroscedasticit and
-%   homoscedasticit residual vectors. 
+%   (mode). The HCCM matrices are valid for both heteroscedastic and
+%   homoscedastic residual vectors. 
 %
 %  Input:
-%    J         Jacobian (NxM matrix)
+%    J         Jacobian (NxM matrix), with
+%                  N = number of data points / variables
+%                  M = number of parameters
 %    res       vector of residuals (Nx1 array)
 %    mode      HCCM estimator (string)
 %                 'HC0' - White, H. (1980)
@@ -29,71 +31,67 @@ function C = hccm(J,res,mode)
 % REFERENCES: 
 % [1] White, H. (1980). A heteroskedasticity-consistent covariance matrix
 %     estimator and a direct test for heteroskedasticity. Econometrica, 48(4), 817-838
-%     DOI: 10.2307/1912934
+%     https://doi.org/10.2307/1912934
 %
 % [2] MacKinnon and White, (1985). Some heteroskedasticity-consistent covariance
 %     matrix estimators with improved finite sample properties. Journal of Econometrics, 
-%     29 (1985), pp. 305-325. DOI: 10.1016/0304-4076(85)90158-7
+%     29 (1985), pp. 305-325.
+%     https://doi.org/10.1016/0304-4076(85)90158-7
 %
 % [3] Davidson and MacKinnon, (1993). Estimation and Inference in Econometrics
 %     Oxford University Press, New York. 
 %
 % [4] Cribari-Neto, F. (2004). Asymptotic inference under heteroskedasticity of
 %     unknown form. Computational Statistics & Data Analysis, 45(1), 215-233
-%     DOI: 10.1016/s0167-9473(02)00366-3
+%     https://doi.org/10.1016/s0167-9473(02)00366-3
 %
 % [5] Cribari-Neto, F., Souza, T. C., & Vasconcellos, K. L. P. (2007). Inference
 %     under heteroskedasticity and leveraged data. Communications in Statistics –
-%     Theory and Methods, 36(10), 1877-1888. DOI: 10.1080/03610920601126589
+%     Theory and Methods, 36(10), 1877-1888.
+%     https://doi.org/10.1080/03610920601126589
+
+% Number of variables
+N = size(J,1);
+% Number of parameters
+M = size(J,2);
 
 if nargin<3 || isempty(mode) || all(size(res)>1)
-    V = res;
+    diagV = res;
 else
-    V = [];
+    diagV = [];
 end
 
 pinvJTJ = pinv(J.'*J);
-% Hat matrix
-H = J*pinvJTJ*J.';
-% Get leverage
-h = diag(H);
-% Number of parameters
-k = size(J,2);
-% Number of variables
-n = size(J,1);
+% Get leverage (diagonal of hat matrix)
+%H = J*pinvJTJ*J.'; h = diag(H);  % consumes too much memory if 
+h = sum((J*pinvJTJ).*J,2);
 
-if isempty(V)
+if isempty(diagV)
     % Select estimation method using established nomenclature
+    % to estimate the diagonal of the data covariance matrix
     switch upper(mode)
         
         case 'HC0' % White,(1980),[1]
-            % Estimate the data covariance matrix
-            V = diag(res.^2);
+            diagV = res.^2;
             
         case 'HC1' % MacKinnon and White,(1985),[2]
-            % Estimate the data covariance matrix
-            V = n/(n-k)*diag(res.^2);
+            diagV = N/(N-M)*res.^2;
             
         case 'HC2' % MacKinnon and White,(1985),[2]
-            % Estimate the data covariance matrix
-            V = diag(res.^2./(1-h));
+            % Estimate the diagonal of the data covariance matrix
+            diagV = res.^2./(1-h);
             
         case 'HC3' % Davidson and MacKinnon,(1993),[3]
-            % Estimate the data covariance matrix
-            V = diag(res./(1-h)).^2;
+            diagV = (res./(1-h)).^2;
             
         case 'HC4' % Cribari-Neto,(2004),[4]
-            % Compute discount factor
-            delta = min(4,n*h/k);
-            % Estimate the data covariance matrix
-            V = diag(res.^2./((1 - h).^delta));
+            delta = min(4,N*h/M);  % discount factor
+            diagV = res.^2./((1 - h).^delta);
             
         case 'HC5' % Cribari-Neto,(2007),[5]
-            % Compute inflation factor
-            k = 0.7;
-            alpha = min(max(4,k*max(h)/mean(h)),h/mean(h));
-            % Estimate the data covariance matrix
-            V = diag(res.^2./(sqrt((1 - h).^alpha)));
+            M = 0.7;
+            alpha = min(max(4,M*max(h)/mean(h)),h/mean(h));  % inflation factor
+            diagV = res.^2./(sqrt((1 - h).^alpha));
             
         otherwise
             error('HCCM estimation mode not found.')
@@ -101,6 +99,6 @@ if isempty(V)
 end
 
 % Heteroscedasticity Consistent Covariance Matrix (HCCM) estimator
-C = pinvJTJ*J.'*V*J*pinvJTJ;
+C = pinvJTJ*J.'.*diagV(:).'*J*pinvJTJ;
 
 end
