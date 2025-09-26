19920b1eceb9c15cf5503eac25dce0a103fee36c
Gianluca Marcozzi <gianluca.marcozzi@web.de>
2025-09-26 11:30:48 +0200

    Create separate test for basecorr 2D mask

--- tests/basecorr_twodim.m
+++ tests/basecorr_twodim.m
@@ -2,24 +2,12 @@ function ok = test()
 
 % Test two-dimensional polynomial baseline correction
 
-x = linspace(0,1,201).';
-y = linspace(0,1,101);
-data0 = gaussian(x,0.4,0.1)*gaussian(y,0.6,0.1);
-baseline = 1 + 0.12*x - 0.9*y + x.^2 + 0.445*y.^2 - 0.2*x.*y;
-
-data = data0 + baseline;
+x = linspace(0,1,7).';
+y = linspace(0,1,6);
+data = 1 + 0.12*x - 0.9*y + x.^2 + 0.445*y.^2 - 0.2*x.*y;
 
 dim = [];
 n = [2 2];
-baselineCheck = basecorr(baseline,dim,n);
+datacorr = basecorr(data,dim,n);
 
-regionx = x<0.1 | x>0.7;
-regiony = y<0.2 | y>0.9;
-region = regionx | regiony;
-
-[datac, baselinec] = basecorr(data, dim, n, region);
-
-ok(1) = max(abs(baselineCheck), [], 'all')<1e-10;
-threshold = 1e-10;
-ok(2) = areequal(datac,data0,threshold,'abs');
-ok(3) = areequal(baseline,baselinec,threshold,'abs');
+ok = max(abs(datacorr))<1e-10;
---
+++ tests/basecorr_twodim_mask.m
@@ -0,0 +1,25 @@
+function ok = test()
+
+% Test two-dimensional polynomial baseline correction and 2D mask
+
+x = linspace(0,1,201).';
+y = linspace(0,1,101);
+data0 = gaussian(x,0.4,0.1)*gaussian(y,0.6,0.1);
+baseline = 1 + 0.12*x - 0.9*y + x.^2 + 0.445*y.^2 - 0.2*x.*y;
+
+data = data0 + baseline;
+
+dim = [];
+n = [2 2];
+baselineCheck = basecorr(baseline,dim,n);
+
+regionx = x<0.1 | x>0.7;
+regiony = y<0.2 | y>0.9;
+region = regionx | regiony;
+
+[datac,baselinec] = basecorr(data,dim,n,region);
+
+ok(1) = max(abs(baselineCheck(:)))<1e-10;
+threshold = 1e-10;
+ok(2) = areequal(datac,data0,threshold,'abs');
+ok(3) = areequal(baseline,baselinec,threshold,'abs');
