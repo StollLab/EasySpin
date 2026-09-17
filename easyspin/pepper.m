94306839c1de7d238e74b5f1c5a80f1b622cc323
Stefan Stoll <stst@uw.edu>
2026-09-16 22:14:41 -0700

    pepper: error for Gaussian-only dispersion (closes #333)

--- easyspin/pepper.m
+++ easyspin/pepper.m
@@ -837,7 +837,7 @@ elseif ~BruteForceSum
       wT = x0T/2.5;  % width; results in <2e-9 at borders for mwPhase==0
       xT = 0:2*x0T-1;  % needs to be zero-based and with increment 1 (for lisum1i)
       if Exp.mwPhase~=0 && Sys.lw(2)==0
-        disp('Exp.mwPhase is disregarded for Gaussian-only spectrum. Add a Lorentzian component.')
+        error('Dispersion spectrum with Gaussian-only broadening is not implemented. Add a Lorentzian component.')
       end
       Template = gaussian(xT,x0T,wT,-1);
     else
